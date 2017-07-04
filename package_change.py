980422dfc5347e935ef85df4aa18325c9297517c
Vineet Bhatia <vbhatia@groupon.com>
2017-07-03 17:23:49 -0700

    added functionality for pkg update and new pkg

--- package_change.py
+++ package_change.py
@@ -5,12 +5,55 @@ import re
 import os
 import datetime
 import commands
+import shutil
+
+# Updating hostclass with a new pkg or new version of an existng pkg
+
+def update_hostclass_new_pkg(file_path, pkg_full, hostclass, path_to_ops_config_queue):
+  timestamp = datetime.datetime.today().strftime('%Y.%m.%d_%H.%M')
+  cmd_commit = 'git commit -m "add %s" %s' % (pkg_full, file_path)
+  cmd_push = 'git pull --rebase && ' + path_to_ops_config_queue
+  cmd_tag = 'git tag %s-%s' % (hostclass, timestamp)
+  cmd_push_tag = 'git push --tags'
+  cmd_git_diff = 'git diff'
+  cmd_git_status = 'git status'
+  cmd_revert = 'git checkout %s' % file_path
+      
+  f = open(file_path, 'r')
+  f_new = open('hostclass_tmp', 'w')
+  for line in f:
+    f_new.write(line)
+    if 'production' in line:
+      f_new.write('  - ' + pkg_full + '\n')
+  f.close()
+  f_new.close()
+  shutil.move('hostclass_tmp', file_path)
+
+  (status, output) = commands.getstatusoutput(cmd_git_status)
+  (diff_status, diff_output) = commands.getstatusoutput(cmd_git_diff)
+  
+  print output
+  print '\n'
+  print diff_output
+
+  commit_ans = raw_input('Ready to commit? y/n: ')
+  if commit_ans == 'n':
+    revert_ans = raw_input('Revert the changes? y/n: ')
+    if revert_ans == 'y':
+      (status, output) = commands.getstatusoutput(cmd_revert)
+      if status:    ## Error case, print the command's output to stderr and exit
+        sys.stderr.write(output)
+        sys.exit(1)
+      print output  ## Otherwise do something with the command's output    
+
+  if commit_ans == 'y':
+    create_new_hostclass_git_tag(cmd_commit, cmd_push, cmd_tag, cmd_push_tag)    
 
 
-def update_hostclass(file_path, pkg_base, pkg_full, hostclass, path_to_ops_config_queue):
+def update_hostclass_new_version(file_path, pkg_base, pkg_full, hostclass, path_to_ops_config_queue):
   timestamp = datetime.datetime.today().strftime('%Y.%m.%d_%H.%M')
   cmd_commit = 'git commit -m "bump %s" %s' % (pkg_base.group(), file_path)
-  cmd_push = path_to_ops_config_queue
+  cmd_push = 'git pull --rebase &&' + path_to_ops_config_queue
   cmd_tag = 'git tag %s-%s' % (hostclass, timestamp)
   cmd_push_tag = 'git push --tags'
   cmd_git_diff = 'git diff'
@@ -26,7 +69,8 @@ def update_hostclass(file_path, pkg_base, pkg_full, hostclass, path_to_ops_confi
       data = data.replace(match.group(), pkg_full)
       f = open(file_path, 'w')
       f.write(data)
-      f.close() 
+      f.close()
+  
   (status, output) = commands.getstatusoutput(cmd_git_status)
   (diff_status, diff_output) = commands.getstatusoutput(cmd_git_diff)
   
@@ -61,16 +105,25 @@ def create_new_hostclass_git_tag(cmd_commit, cmd_push, cmd_tag, cmd_push_tag):
 def main():
   args = sys.argv[1:]
   if not args:
-    print ('Usage: <script_name> <hostclass> <pkg_name>')
+    print ('Usage: <script_name> <hostclass> <pkg_name> [--new] [--update]')
     sys.exit(1)
-
-  pkg_full = args[1]
-  pkg_base = re.search('\w+', pkg_full)
-  hostclass = args[0]
-  path_to_ops_config_queue = os.path.expanduser('~') + '/ops-config/bin/ops-config-queue'
-  path_to_hostclass_yml = os.path.expanduser('~') + '/ops-config/hostclasses/%s.yml' % hostclass
   
-  update_hostclass(path_to_hostclass_yml, pkg_base, pkg_full, hostclass, path_to_ops_config_queue )
+  if args[2] == '--update':
+    pkg_full = args[1]
+    pkg_base = re.search('\w+', pkg_full)
+    hostclass = args[0]
+    path_to_ops_config_queue = os.path.expanduser('~') + '/ops-config/bin/ops-config-queue'
+    path_to_hostclass_yml = os.path.expanduser('~') + '/ops-config/hostclasses/%s.yml' % hostclass
+  
+    update_hostclass_new_version(path_to_hostclass_yml, pkg_base, pkg_full, hostclass, path_to_ops_config_queue)
+  
+  elif args[2] == '--new':
+    pkg_full = args[1]
+    hostclass = args[0]
+    path_to_ops_config_queue = os.path.expanduser('~') + '/ops-config/bin/ops-config-queue'
+    path_to_hostclass_yml = os.path.expanduser('~') + '/ops-config/hostclasses/%s.yml' % hostclass
+  
+    update_hostclass_new_pkg(path_to_hostclass_yml, pkg_full, hostclass, path_to_ops_config_queue )
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
