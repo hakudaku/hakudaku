3f80a821a4391be4dd0d5290279dd88a6e37983a
Vineet Bhatia <vbhatiav@yahoo.com>
2018-12-11 21:58:10 -0800

    remove files

--- rf_scripts/words.py
+++
@@ -1,7 +0,0 @@
-#!/Users/vineetbhatia/ENV/bin/python -tt
-
-import random
-
-lines = open('words').read().splitlines()
-myline =random.choice(lines)
-print(myline)
--- word_print.py
+++
@@ -1,5 +0,0 @@
-import random
-
-lines = open('words').read().splitlines()
-myline =random.choice(lines)
-print(myline)
