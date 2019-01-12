5e36af0895ec54658d8883cebc566d9d12ed929a
Vineet Bhatia <vbhatiav@yahoo.com>
2019-01-12 14:44:57 -0800

    add new aws script

---
+++ aws_alias_update_single.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import json
+import commands
+import re 
+import os
+import fileinput
+import sys
+
+my_aws_start_cmd = 'for i in `aws ec2 describe-instances | grep InstanceId | awk -F\" \'{print $4}\'`;do aws ec2 start-instances --instance-ids $i;done'
+
+os.system(my_aws_start_cmd)
+
+my_bash = os.path.expanduser('~') + '/.bashrc'
+my_out_file = 'out.txt'
+
+my_cmd = 'aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicDnsName" --output=text'
+
+(status, out) = commands.getstatusoutput(my_cmd)
+
+
+# if line in .bashrc matches the regexs, do the replace operation (only if ip has changed), else copy line as is
+with open(my_bash, 'r') as fin:
+    with open(os.path.expanduser('~') + '/' + my_out_file, 'w') as fout: 
+        for line in fin:
+            match = re.search(r'(ec2.+)"', line)
+            if match:
+                if match.group(1) == out:
+                    print 'IP has not changed. Nothing to do.'
+                    sys.exit(1)
+                if match.group(1) != out:
+                    string_to_replace = match.group(1)
+                    fout.write(line.replace(string_to_replace, out))
+            else:
+                fout.write(line)
+
+
+# Rename out file to .bashrc
+os.rename(os.path.expanduser('~') + '/' + my_out_file, my_bash)
+
+# Re-read .bashrc
+output = commands.getoutput('source' + ' ' + my_bash)
--- smtp_email.py
+++ smtp_email.py
@@ -1,38 +1,43 @@
-#!/usr/bin/python
+#!/Users/vineetbhatia/ENV/bin/python -tt
 
 import sys
 import smtplib
 import dns.resolver
 from socket import error
+import re
 
 def smtp(sender, rcpt, server, subject, body):
+    print server
     msg = 'From: {}\nTo:{}\nSubject: {}\n{}'.format(sender, rcpt, subject, body)
     try:
         smtpObj = smtplib.SMTP(server)
-        smtpObj.sendmail(sender, rcpt, message)
+        smtpObj.sendmail(sender, rcpt, msg)
     except error:
-        print 'Send failed'
-    except smtplib.SMTPException:
-        print 'Send failed'
+        print 'send failed' 
+    except smtplib.SMTPException as e:
+        print str(e) 
     else:
-        print 'Successfully sent email for %s.' % bg
+        print 'Successfully sent email' 
 
 
 def main():
     args = sys.argv[1:]
     email_addr = raw_input('Enter email addresses seperated by commas: ')
+    email_sender = raw_input('Enter sender: ')
+    email_sub = raw_input('Enter subject: ') 
+    email_body = raw_input('Enter body: ') 
+
     search = re.search(r'.+@(.+)', email_addr)
     domain = search.group(1)
     #email_addr_list = email_addr.split(',')
-    email_sender = args[1]
-    email_sub = args[3]
-    email_body = args[5]
-    
     answers = dns.resolver.query(domain, 'MX')
     mx_list = []
     for rdata in answers:
-        mx_list.append(x)
-    mx = mx_list[0]
+        mx_list.append(rdata)
+    mx = str(mx_list[0])
+    mx_list = mx.split(' ') 
+    mx = mx_list[1] 
+    
 
     smtp(email_sender, email_addr, mx, email_sub, email_body)
 
