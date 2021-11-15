ce000bae6eaa8a6f363b80a863abf1d69c8ed45c
Vineet Bhatia <vbhatia@authentic8.com>
2021-11-15 12:57:14 -0800

    add comments

--- remove_strings_from_file.py
+++ remove_strings_from_file.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+# Compare file1 and file2. Final file3 will have all lines from file2 minus lines in file1.
+
 list_of_removed_hosts = []
-with open('ass', 'r') as f:
+with open('file1', 'r') as f:
     for line in f:
         #new_line = line.strip('\n')
         list_of_removed_hosts.append(line)
 
-with open('ass1', 'r') as f:
-    with open('prod_new', 'a') as f2:
+with open('file2', 'r') as f:
+    with open('file3', 'a') as f2:
         for line in f:
             if line in list_of_removed_hosts:
                 pass
             else:
-                f2.write(line)
+                f2.write(line) # only write lines NOT in file1 to the new file (file3)
 
