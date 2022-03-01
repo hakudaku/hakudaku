71a4402d1da358ee47e93b32745083d314f611cb
Vineet Bhatia <vbhatiav@yahoo.com>
2022-02-28 20:29:48 -0800

    add new scripts

--- bootstrap_fips.py
+++ bootstrap_fips.py
@@ -7,23 +7,28 @@ import os
 import time
 from shutil import copy
 import glob
+import base64
+from getpass import getpass
 
-def add_fw_rules(hostname, region, ticket):
+def add_fw_rules(hostname, region, ticket, env, decoded_password, similar_host):
     get_ip_cmd = 'dig a {} +short'.format(hostname)
     (status, output) = commands.getstatusoutput(get_ip_cmd)
     ip_list = output.split()
     ip = ip_list[0]
-    iptables_cmd = 'sudo iptables -A INPUT -s "{}/32" -p tcp -m multiport --dports 443 -m comment --comment "allow_{}_32_to_443" -j ACCEPT && sudo iptables -L -n |egrep {}'.format(ip, ip, ip)
+    iptables_cmd = 'echo {} | sudo -S iptables -A INPUT -s "{}/32" -p tcp -m multiport --dports 443 -m comment --comment "allow_{}_32_to_443" -j ACCEPT && sudo iptables -L -n |egrep {}'.format(decoded_password, ip, ip, ip)
     knife_ssh_cmd1 = 'knife ssh -t 15 "role:chef-server" "{}"'.format(iptables_cmd)
     knife_ssh_cmd2 = 'knife ssh -t 15 "role:resolver-prod" "{}"'.format(iptables_cmd)
     knife_ssh_cmd3 = 'knife ssh -t 15 ""role:repo-prod "{}"'.format(iptables_cmd)
     os.system(knife_ssh_cmd1)
     os.system(knife_ssh_cmd2)
     os.system(knife_ssh_cmd3)
-    get_vault_items(hostname, region, ticket)
+    get_vault_items(hostname, region, ticket, env, decoded_password, similar_host)
 
-def get_vault_items(hostname, region, ticket):
-    get_runlist_cmd = 'knife node show {}'.format(hostname)
+def get_vault_items(hostname, region, ticket, env, decoded_password, similar_host):
+    if similar_host == None:
+        get_runlist_cmd = 'knife node show {}'.format(hostname) # Get runlist parameters from exising/reimaged chef node
+    else:
+        get_runlist_cmd = 'knife node show {}'.format(similar_host) # Get runlist parameters from a similar chef node
     (status, output) = commands.getstatusoutput(get_runlist_cmd)
     match = re.search(r'Run List:\s+(.+])', output)
     app_runlist = match.group(1)
@@ -33,9 +38,12 @@ def get_vault_items(hostname, region, ticket):
     (status, output) = commands.getstatusoutput(vault_item_cmd)
     vault_item_list = output.split()
     vault_item_str = ' '.join(vault_item_list)
-    delete_from_chef(hostname, vault_item_str, app_runlist, region, ticket)
+    if similar_host == None:
+        delete_from_chef(hostname, vault_item_str, app_runlist, region, ticket, env, decoded_password)
+    else:
+        bootstrap(hostname, vault_item_str, app_runlist, region, ticket, env, decoded_password)    
 
-def delete_from_chef(hostname, vault_item_str, app_runlist, region, ticket):
+def delete_from_chef(hostname, vault_item_str, app_runlist, region, ticket, env, decoded_password):
     chef_file_backup_path = os.path.expanduser('~') + '/decoms/decom_host_backup_files'
     backup_chef_node_cmd = 'knife node show -F json {} > {}.json'.format(hostname, hostname)
     backup_client_key_cmd = 'knife client key show -F json {} default > {}.pub'.format(hostname, hostname)
@@ -56,16 +64,16 @@ def delete_from_chef(hostname, vault_item_str, app_runlist, region, ticket):
     for f in chef_client_key_backup_file:
         copy(f, chef_file_backup_path)
 
-    bootstrap(hostname, vault_item_str, app_runlist, region, ticket)
+    bootstrap(hostname, vault_item_str, app_runlist, region, ticket, env, decoded_password)
 
-def bootstrap(hostname, vault_item_str, app_runlist, region, ticket):
+def bootstrap(hostname, vault_item_str, app_runlist, region, ticket, env, decoded_password):
     gce_bootstrap_key_path = os.path.expanduser('~') + '/.ssh/gce_bootstrap'
     deb_pkg_path = os.path.expanduser('~') + '/fips-chef_12.22.5-2_amd64.deb'
     bootstrap_cmd = 'knife bootstrap  -y -x chef -i {} -E prod -N {} --json-attributes \'{{"authentic8":{{"use_apt_mirrors": false}}}}\' --bootstrap-version 12 -r role[common] --sudo --no-fips {} {}'.format(gce_bootstrap_key_path, hostname, vault_item_str, hostname)
     add_to_runlist_cmd = 'knife node run_list add {} \'{}\''.format(hostname, app_runlist)
     scp_cmd = 'scp {} {}:~'.format(deb_pkg_path, hostname)
-    install_deb_pkg_cmd = 'knife ssh -m {} "sudo apt install ~/fips-chef_12.22.5-2_amd64.deb" -t 15 --no-host-key-verify'.format(hostname) 
-    run_chef_cmd = 'knife ssh -m {} "sudo -i chef-client" -t 15 --no-host-key-verify'.format(hostname)
+    install_deb_pkg_cmd = 'knife ssh -m {} "echo {} | sudo -S apt install ~/fips-chef_12.22.5-2_amd64.deb" -t 15 --no-host-key-verify'.format(hostname, decoded_password) 
+    run_chef_cmd = 'knife ssh -m {} "echo {} | sudo -Si chef-client" -t 15 --no-host-key-verify'.format(hostname, decoded_password)
 
     # Run commands
     os.system(bootstrap_cmd)
@@ -73,7 +81,7 @@ def bootstrap(hostname, vault_item_str, app_runlist, region, ticket):
     os.system(scp_cmd)
     os.system(install_deb_pkg_cmd)
     os.system(run_chef_cmd)
-    apply_updates_and_reboot_cmd = 'knife ssh -m {} "sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::=--force-confnew --with-new-pkgs upgrade && sudo systemctl reboot --force"'.format(hostname)
+    apply_updates_and_reboot_cmd = 'knife ssh -m {} "echo {} | sudo -S apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::=--force-confnew --with-new-pkgs upgrade && sudo systemctl reboot --force"'.format(hostname, decoded_password, decoded_password)
     os.system(apply_updates_and_reboot_cmd)
     time.sleep(300)
     os.system(run_chef_cmd)
@@ -83,16 +91,30 @@ def bootstrap(hostname, vault_item_str, app_runlist, region, ticket):
 
 def main():
     parser = argparse.ArgumentParser(description="Bootstrap servers")
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument("hostname", help="FQDN of host being bootstrapped")
     parser.add_argument("region", help="Region tag of host being bootstrapped")
     parser.add_argument("ticket", help="Jira ticket")
+    parser.add_argument("env", help="Environment of the host being Bootstrap")
+    group.add_argument("-n", "--new", help="New host", action="store_true") # Bootstrapping a new instance (e.g. not an existing/reimaged host)
 
     args = parser.parse_args()
     hostname = args.hostname
     region = args.region
     ticket = args.ticket
+    env = args.env
+    
+    #sudo_password = raw_input("sudo password:\n")
+    sudo_password = getpass.getpass()
+    encoded_password = base64.b64encode(sudo_password.encode("utf-8"))
+    decoded_password = base64.b64decode(encoded_password).decode("utf-8")
 
-    add_fw_rules(hostname, region, ticket)
+    if args.new:
+        similar_host =  raw_input("hostname:\n")# Since it is a new instance, get the runlist parameters from a user provided similar host 
+    else:
+        similar_host = None
+        
+    add_fw_rules(hostname, region, ticket, env, decoded_password, similar_host)
 
 
 # This is the standard boilerplate that calls the main() function.
